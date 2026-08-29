eedeca91349afdb11c980b0820b537103852de43
Michael Bestas <mkbestas@lineageos.org>
2026-08-29 09:03:01 +0300

    sm6375-common: Drop no longer used TARGET_KERNEL_NO_GCC
    See: I419ffd8c4f055ca29a6554983ed44aaaab54a666
    Change-Id: I9ce189a991bddd4ec9e42f6853fe5fcd12b51038

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -47,7 +47,6 @@ BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
 BOARD_RAMDISK_USE_LZ4 := true
 TARGET_KERNEL_CLANG_VERSION := r563880c
 TARGET_KERNEL_CLANG_PATH := $(abspath .)/prebuilts/clang/host/linux-x86/clang-$(TARGET_KERNEL_CLANG_VERSION)
-TARGET_KERNEL_NO_GCC := true
 TARGET_KERNEL_SOURCE := kernel/motorola/sm6375
 TARGET_KERNEL_CONFIG := vendor/holi-qgki_defconfig vendor/ext_config/lineage_moto-holi.config
 
